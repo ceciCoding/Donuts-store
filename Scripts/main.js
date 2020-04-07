a23c7a7d28a8aa43dc3bfb70e6d8bd796cca0851
Cecilia Olivera <ceci.coding@gmail.com>
2020-04-07 19:29:26 +0200

    finished index mobile

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,10 +1,27 @@
 const images = ['../Images/pink.jpg', '../Images/blue.jpg', '../Images/yellow.jpg', '../Images/white.jpg', '../Images/boston.jpg', '../Images/apple.jpg', '../Images/choco.jpg'];
 let counter = 0;
+let plus = document.querySelector('.plus');
+let less = document.querySelector('.less');
+let number = document.querySelector('.number-donuts');
+let numberOfDonuts = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     let container = document.querySelector('.slider');
     slide(container);
 });
+
+plus.addEventListener('click', () => {
+    number.value = (numberOfDonuts + 1);
+    numberOfDonuts++;
+});
+
+less.addEventListener('click', () => {
+    if (number.value <= 0) return;
+    else {
+        number.value = (numberOfDonuts - 1);
+        numberOfDonuts--;
+    }
+})
 
 function slide(container) {
     container.addEventListener('click', e => {
