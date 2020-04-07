56e60592624826458c74e6870eda478da9481e67
Cecilia Olivera <ceci.coding@gmail.com>
2020-04-07 14:18:30 +0200

    slider for homepage

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,34 +1,35 @@
-const donutImages = ["../Images/pink", "../Images/blue", "../Images/choco", "../Images/apple", "../Images/boston", "../Images/yellow", "../Images/white"]
+const images = ['../Images/pink.jpg', '../Images/blue.jpg', '../Images/yellow.jpg', '../Images/white.jpg', '../Images/boston.jpg', '../Images/apple.jpg', '../Images/choco.jpg'];
 let counter = 0;
 
-document.addEventListener("DOMContentLoaded", () => {
-    let container = document.querySelector("carousel");
-    carousel(container);
-})
+document.addEventListener('DOMContentLoaded', () => {
+    let container = document.querySelector('.slider');
+    slide(container);
+});
 
-function carousel(container) {
+function slide(container) {
     container.addEventListener('click', e => {
-        const backArrow = container.querySelector("back-arrow");
-        const forwardArrow = container.querySelector("forward-arrow");
-        const img = container.querySelector("img");
-        const target = e.target;
-
+        const backArrow = container.querySelector('.back-arrow');
+        const forwardArrow = container.querySelector('.forward-arrow');
+        let img = container.querySelector('img');
+        let target = e.target;
+        
         if (target === backArrow) {
             if (counter > 0) {
-                img.src = donutImages[counter - 1];
+                img.src = images[counter - 1];
                 counter--;
             } else {
-                img.src = donutImages[donutImages.length - 1];
-                counter = donutImages.length - 1;
+                img.src = images[images.length - 1];
+                counter = images.length - 1;
             }
-        } else if (target === forwardArrow){
-            if (counter < donutImages.length - 1) {
-                img.src = donutImages[counter + 1];
+        } else if (target === forwardArrow) {
+            if (counter < images.length - 1) {
+                img.src = images[counter + 1];
                 counter++;
             } else {
-                img.src = donutImages[0];
+                img.src = images[0];
                 counter = 0;
             }
-        }
-    })
+        };
+    });
 }
+
